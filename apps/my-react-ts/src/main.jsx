fb620f408066466dffa7166e1b569716ad350bc1
sahilm2 <sahil.mandhare@fyntune.com>
2025-07-04 12:57:36 +0530

    f

--- apps/my-react-ts/src/main.jsx
+++ apps/my-react-ts/src/main.jsx
@@ -7,3 +7,29 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>
 );
+// let ques = ["name.school.name", "name.college.test"]
+// let ans = {}
+
+// for (let i = 0; i < ques.length; i++) {
+//     let arr = ques[i].split(".")
+//     ans = combine(arr[0], arr.splice(1), ans)
+// }
+
+// function combine(s, remain, some) {
+//     if (!s && !some) return 0
+    
+//     if (some && Object.keys(some).length && some.hasOwnProperty(s)) {
+//         let b = some[s]
+//         let n = combine(remain[0], remain.splice(1), b) 
+//         return {[s]: {
+//         ...b,
+//         ...n
+//     }}
+//     }
+    
+//     return {
+//         [s]: combine(remain[0], remain.splice(1)) 
+//     }
+// }
+
+// console.log(ans)
